205dd82dea7b975cb5f65465182df8574d140156
JonaVS <jon4963@gmail.com>
2023-03-09 16:32:07 -0600

    feat: add custom height option to Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { height: {
+      shortenerSectionH: 'calc(100vh - 120px)',
+    },},
   },
   plugins: [],
 }
