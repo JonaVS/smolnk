80200ed796d9a4dc824312e9267d45a1a74760de
JonaVS <jon4963@gmail.com>
2023-03-12 01:55:41 -0600

    feat: add useShortenerForm hook
    -Custom hook that handles ShortenerForm logic
    -Kinda messy, gonna looks for ways to improve it later on. ⚗️💩

---
+++ src/components/ShortenerSection/ShortenerSectionForm/hooks/useShortenerForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Url } from "../../../../models/Url";
+import { ApiResult } from "../../../../types/ApiResult";
+import apiAgent from "../../../../api/agent";
+import { getApiError } from "../../../../utils/errorExtractor";
+
+export const useShortenerForm = () => {
+  const [urlInputVal, setUrlInputVal] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiResult, setApiResult] = useState<ApiResult<Url>>({data: null, error: null})
+
+  const handleUrlInputChange = ( event: React.ChangeEvent<HTMLInputElement>): void => {
+    setUrlInputVal(event.target.value);
+  };
+
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setApiResult({ data: null, error: null });
+    try {
+      const result = await apiAgent.url.shortenUrl({
+        urlToShorten: urlInputVal,
+      });
+      setApiResult({ data: result, error: null });
+    } catch (error) {
+      setApiResult({ data: null, error: getApiError(error) });
+    }
+    setIsLoading(false);
+  };
+
+  return { urlInputVal, isLoading, apiResult, handleUrlInputChange, handleFormSubmit}
+};
