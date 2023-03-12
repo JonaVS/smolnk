bd1b7b4b7174f60bb8fa0abb0f3c9edb50227ab6
JonaVS <jon4963@gmail.com>
2023-03-12 01:47:31 -0600

    feat: add API agent
    -This defines the API endpoints that the app is going to use

---
+++ src/api/agent.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from "axios";
+import { ShortenerFormValues, Url } from "../models/Url";
+
+const response = <T>(response: AxiosResponse<T>) => response.data;
+axios.defaults.baseURL = import.meta.env.VITE_SHORTENER_API_BASE_URL;
+
+/*-----DEV MODE ONLY CODE START-----*/
+//This is just to simulate some dalay of requests on development
+if (import.meta.env.DEV) {
+  const sleep = (delay: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, delay);
+    });
+  };
+
+  axios.interceptors.request.use(async (response) => {
+    await sleep(500);
+    return response;
+  });
+}
+/*-----DEV MODE ONLY CODE END-----*/
+
+const url = {
+  shortenUrl: (body: ShortenerFormValues) => axios.post<Url>("/shortener", body).then(response),
+};
+
+const apiAgent = {
+  url,
+};
+
+export default apiAgent;
