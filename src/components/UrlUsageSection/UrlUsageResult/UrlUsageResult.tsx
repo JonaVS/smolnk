01d2eb267b37ea9362078b0782e6ca4af3eee128
JonaVS <jon4963@gmail.com>
2023-03-17 01:53:03 -0600

    feat: add logic to UrlUsageForm

---
+++ src/components/UrlUsageSection/UrlUsageResult/UrlUsageResult.tsx
@@ -0,0 +1,31 @@
+import { UrlUsageCount } from "../../../models/Url";
+import { ApiResult } from "../../../types/ApiResult";
+import LoadingSpinner from "../../shared/LoadingSpinner/LoadingSpinner";
+
+type props = {
+  isLoading: boolean,
+  result: ApiResult<UrlUsageCount>
+}
+
+const UrlUsageResult = ({ isLoading, result }: props) => {
+  return (
+    <div className="h-20 mt-6">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : result.data && result.data.usageCount !== null ? (
+        <div className="w-44 rounded-xl p-2 text-gray-50 font-bold text-center shadow-md">
+          <p>Usage count</p>
+          <span className="font-extrabold text-xl m-auto block">
+            {result.data.usageCount.toLocaleString()}
+          </span>
+        </div>
+      ) : (
+        result.error && <p className="text-white font-bold">{result.error}</p>
+      )}
+    </div>
+  );
+};
+
+
+
+export default UrlUsageResult;
--- src/components/UrlUsageSection/UrlUsageSectionForm/UrlUsageForm.tsx
+++ src/components/UrlUsageSection/UrlUsageSectionForm/UrlUsageForm.tsx
@@ -1,26 +1,48 @@
+import { useApiForm } from "../../../hooks/useApiForm";
+import { UrlUsageCount, UrlUsageFormValues } from "../../../models/Url";
+import apiAgent from "../../../api/agent";
+import { ensureHttpPrefix } from "../../../utils/httpPrefix";
 import TextInput from "../../shared/TextInput/TextInput";
+import UrlUsageResult from "../UrlUsageResult/UrlUsageResult";
+
+const initialValues = {
+  shortUrl: ""
+}
 
 const UrlUsageForm = () => {
+  const { values, isSubmitting, apiResult, handleChange, handleSubmit } =
+    useApiForm<UrlUsageFormValues, UrlUsageCount>({
+      initialValues,
+      cleanOnSuccess: false,
+      onSubmit: async (values): Promise<UrlUsageCount> => {
+        return await apiAgent.url.getUrlUsageCount(ensureHttpPrefix(values.shortUrl));
+      },
+    }); 
+
   return (
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="flex px-3 w-full justify-center"
-    >
-      <TextInput
-        id="shortUrlInput"
-        label="Short URL to search"
-        labelClassName="hidden"
-        className="max-w-xl w-10/12 h-14 px-3 bg-gray-50 text-gray-500 font-medium rounded-l-xl drop-shadow-lg focus:outline-none"
-        placeholder="Ex: https://smolnk.com/wn9H0C"
-      />
-      <button
-        type="submit"
-        className="px-3 text-gray-50 font-bold bg-purple-600 rounded-r-xl drop-shadow-2xl transition-opacity 
+    <>
+      <form onSubmit={handleSubmit} className="flex px-3 w-full justify-center">
+        <TextInput
+          id="shortUrlInput"
+          label="Short URL to search"
+          value={values.shortUrl}
+          name="shortUrl"
+          labelClassName="hidden"
+          className="max-w-xl w-10/12 h-14 px-3 bg-gray-50 text-gray-500 font-medium rounded-l-xl drop-shadow-lg focus:outline-none"
+          placeholder="Ex: https://smolnk.com/wn9H0C"
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+          className="px-3 text-gray-50 font-bold bg-purple-600 rounded-r-xl drop-shadow-2xl transition-opacity 
           duration-200 ease-in-out hover:opacity-75 disabled:opacity-50 disabled:pointer-events-none"
-      >
-        Submit
-      </button>
-    </form>
+          disabled={(values.shortUrl === "" || isSubmitting) ? true : false}
+        >
+          Submit
+        </button>
+      </form>
+      <UrlUsageResult isLoading={isSubmitting} result={apiResult}/>
+    </>
   );
 };
 
