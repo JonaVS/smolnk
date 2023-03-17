d451317408806c8e86f50e72a9b288ce3e3d8011
JonaVS <jon4963@gmail.com>
2023-03-17 00:36:00 -0600

    feat: add useApiForm custom hook
    -Generic hook that is able to handle API related forms

---
+++ src/hooks/useApiForm.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { ApiResult } from "../types/ApiResult";
+import { getApiError } from "../utils/errorExtractor";
+
+type UseApiFormParam<T, Q> = {
+  initialValues: T;
+  cleanOnSuccess?: boolean
+  onSubmit: (values: T) => Promise<Q>;
+};
+
+export const useApiForm = <T extends {}, Q>({ initialValues, onSubmit, cleanOnSuccess = true }: UseApiFormParam<T, Q>) => {
+  const [values, setValues] = useState<T>(initialValues);
+  const [apiResult, setApiResult] = useState<ApiResult<Q>>({data: null, error: null});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setApiResult({ data: null, error: null });
+    try {
+      const result = await onSubmit(values);
+      setApiResult({ data: result, error: null });
+      if (cleanOnSuccess) setValues(initialValues);
+    } catch (error) {
+      setApiResult({ data: null, error: getApiError(error) });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return {
+    values,
+    apiResult,
+    isSubmitting,
+    handleChange,
+    handleSubmit,
+  };
+};
