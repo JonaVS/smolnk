7d3c2d84b73cadb7deed67d572dd323eb3d1b5df
JonaVS <jon4963@gmail.com>
2023-03-20 19:54:34 -0600

    feat: add default styles to TextInput
    -This is just to prevent repetition

--- src/components/shared/TextInput/TextInput.tsx
+++ src/components/shared/TextInput/TextInput.tsx
@@ -9,7 +9,12 @@ const TextInput = ({ label, labelClassName, id, ...props }: props) => {
   return (
     <>
       <label htmlFor={id} className={labelClassName}>{label}</label>
-      <input id={id} type="text" {...props} />
+      <input
+        id={id}
+        type="text"
+        className="max-w-sm md:max-w-lg w-full h-14 px-3 bg-gray-50 text-gray-500 font-medium rounded-l-xl drop-shadow-lg focus:outline-none"
+        {...props}
+      />
     </>
   );
 };
