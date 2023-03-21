0764769db0ed3ed1714526ad5a356692b464d53c
JonaVS <jon4963@gmail.com>
2023-03-20 19:14:26 -0600

    feat: add support for extra styles to MainSectionWrapper

--- src/components/shared/MainSectionWrapper/MainSectionWrapper.tsx
+++ src/components/shared/MainSectionWrapper/MainSectionWrapper.tsx
@@ -5,7 +5,7 @@ type Props = HTMLProps<HTMLDivElement>
 
 const MainSectionWrapper = ({ children, ...props }: Props) => {
   return (
-    <section className="m-auto w-full md:w-3/4 max-w-7xl" {...props}>{children}</section>
+    <section {...props} className={`m-auto w-full md:w-3/4 max-w-7xl ${props.className}`}>{children}</section>
   );
 };
 
