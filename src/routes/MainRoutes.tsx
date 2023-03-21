ab6b913ad421928648763a9d5e48f041ee67dbe9
JonaVS <jon4963@gmail.com>
2023-03-21 15:40:21 -0600

    feat: add base config for page transitions
    -Via framer-motion

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from "react-router-dom";
 import UrlShortener from "../pages/UrlShortener";
 import UrlUsageCount from "../pages/UrlUsageCount";
 import PageNotFound from "../pages/PageNotFound";
 
 const MainRoutes = () => {
+  const location = useLocation()
+  
   return (
-    <Routes>
-      <Route path="/" element={<UrlShortener />} />
-      <Route path="/url-usage" element={<UrlUsageCount />} />
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<UrlShortener />} />
+        <Route path="/url-usage" element={<UrlUsageCount />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
