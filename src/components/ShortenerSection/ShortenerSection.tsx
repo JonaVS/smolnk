ab185e927fa0163375797a54473ab0b5661985ad
JonaVS <jon4963@gmail.com>
2023-03-21 16:09:06 -0600

    feat: add transition to ShortenerSection

--- src/components/ShortenerSection/ShortenerSection.tsx
+++ src/components/ShortenerSection/ShortenerSection.tsx
@@ -4,7 +4,12 @@ import ShortenerForm from "./ShortenerSectionForm/ShortenerForm";
 
 const ShortenerSection = () => {
   return (
-    <MainSectionWrapper>
+    <MainSectionWrapper
+      transition={{ duration: 0.5, type: "tween" }}
+      initial={{ x: -100, opacity: 0 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ x: -100, opacity: 0 }}
+    >
       <ShortenerSectionMsg />
       <ShortenerForm />
     </MainSectionWrapper>
