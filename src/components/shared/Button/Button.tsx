020bf14449c2ff74fb99869e3ecf489c70c6a256
JonaVS <jon4963@gmail.com>
2023-03-21 15:05:00 -0600

    feat: add custom Button component

---
+++ src/components/shared/Button/Button.tsx
@@ -0,0 +1,22 @@
+
+export const ButtonVariant = {
+  default: 'p-3 text-gray-50 bg-purple-600 rounded-lg',
+  formPrimary: "px-3 text-gray-50 font-bold bg-purple-600 rounded-r-xl drop-shadow-2xl transition-opacity duration-200 ease-in-out hover:opacity-75 disabled:opacity-50 disabled:pointer-events-none",
+} as const
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?:  keyof typeof ButtonVariant
+};
+
+const Button = ({variant = "default", ...props}:Props) => {
+  return (
+    <button
+    {...props}
+      className={`${ButtonVariant[variant]} ${props.className}`}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
