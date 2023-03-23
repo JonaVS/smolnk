d8e7cbf005effa3e43c7deb0aacea24b61c34e19
JonaVS <jon4963@gmail.com>
2023-03-22 20:51:45 -0600

    ui: animate ShortenerResult

--- src/components/ShortenerSection/ShortenerResult/ShortenerResult.tsx
+++ src/components/ShortenerSection/ShortenerResult/ShortenerResult.tsx
@@ -1,4 +1,5 @@
 import { Url } from "../../../models/Url";
+import { motion, Variants } from "framer-motion";
 import { Result } from "../../../types/Result";
 import LoadingSpinner from "../../shared/LoadingSpinner/LoadingSpinner";
 import ShortenerResultActions from "./ShortenerResultActions/ShortenerResultActions";
@@ -8,20 +9,41 @@ type props = {
   result: Result<Url | null>;
 };
 
+const resultContainer: Variants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { duration: 1 } },
+};
+
+const resultText: Variants = {
+  hidden: { opacity: 0, scale: 0.6 },
+  show: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+}
+
 const ShortenerResult = ({ isLoading, result }: props) => {
   return (
-    <div className="mt-6 h-20 w-max mx-auto text-white font-bold">
+    <motion.div
+      className="mt-6 h-20 w-max mx-auto text-white font-bold"
+      variants={resultContainer}
+      initial="hidden"
+      animate="show"
+    >
       {isLoading ? (
         <LoadingSpinner />
       ) : (
         <>
-          <p>{result.data?.shortUrl ? result.data.shortUrl : result.error}</p>
+          <motion.p
+            variants={resultText}
+            initial="hidden"
+            animate="show"
+          >
+            {result.data?.shortUrl ? result.data.shortUrl : result.error}
+          </motion.p>
           {result.data?.shortUrl && (
             <ShortenerResultActions shortUrl={result.data.shortUrl} />
           )}
         </>
       )}
-    </div>
+    </motion.div>
   );
 };
 
