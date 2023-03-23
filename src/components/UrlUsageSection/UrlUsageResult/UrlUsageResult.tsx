eafcef93715aa7a4fb5a281494fac0c3e53e75a0
JonaVS <jon4963@gmail.com>
2023-03-23 12:57:43 -0600

    ui: animate UrlUsageResult

--- src/components/UrlUsageSection/UrlUsageResult/UrlUsageResult.tsx
+++ src/components/UrlUsageSection/UrlUsageResult/UrlUsageResult.tsx
@@ -1,3 +1,4 @@
+import { Variants, motion } from "framer-motion";
 import { UrlUsageCount } from "../../../models/Url";
 import { Result } from "../../../types/Result";
 import LoadingSpinner from "../../shared/LoadingSpinner/LoadingSpinner";
@@ -8,20 +9,42 @@ type Props = {
   result: Result<UrlUsageCount>
 }
 
+const resultContainer: Variants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { duration: 1 } },
+};
+
+const resultError: Variants = {
+  hidden: { opacity: 0, scale: 0.6 },
+  show: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+}
+
 const UrlUsageResult = ({ isLoading, result }: Props) => {
   return (
-    <div className="h-20 mt-6 text-center">
+    <motion.div
+      className="h-20 mt-6 text-center"
+      variants={resultContainer}
+      initial="hidden"
+      animate="show"
+    >
       {isLoading ? (
         <LoadingSpinner />
       ) : result.data && result.data.usageCount !== null ? (
         <UrlUsage usageCount={result.data.usageCount.toLocaleString()}/>
       ) : (
-        result.error && <p className="text-white font-bold">{result.error}</p>
+        result.error && (
+          <motion.p
+            variants={resultError}
+            initial="hidden"
+            animate="show"
+            className="text-white font-bold"
+          >
+            {result.error}
+          </motion.p>
+        )
       )}
-    </div>
+    </motion.div>
   );
 };
-
-
 
 export default UrlUsageResult;
