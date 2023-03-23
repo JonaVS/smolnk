d6bcd5db14d5e002582ac4338e06fd4f95ebece5
JonaVS <jon4963@gmail.com>
2023-03-23 14:14:16 -0600

    ui: update main tag styles at Layout
    -Top padding of 64px (consideration for fixed navbar)
    -Bottom padding of 40px (To have some space between content and Footer)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({children}:Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex flex-col grow overflow-x-hidden">{children}</main>
+      <main className="flex flex-col grow overflow-x-hidden pt-16 pb-10">{children}</main>
       <Footer />
     </div>
   );
