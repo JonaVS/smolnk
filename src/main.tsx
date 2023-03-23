bad92012f0157f1246807220a0f4a2067d08678e
JonaVS <jon4963@gmail.com>
2023-03-23 14:32:41 -0600

    feat: add scroll to top on page change
    -Using custom component for this
    -Provided component from react-router-dom doesnt work with current setup
    -Added a 500ms delay for scroll restoration due to page transitions

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import MainRoutes from "./routes/MainRoutes";
+import ScrollToTop from "./components/shared/ScrollToTop/ScrollToTop";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <Layout>
         <MainRoutes />
       </Layout>
