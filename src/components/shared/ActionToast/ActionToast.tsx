6415d7921434b05f05c8bb8d400d6fa24f74d2cb
JonaVS <jon4963@gmail.com>
2023-03-25 14:14:53 -0600

    feat: add ActionToast component
    -Small component that act as a toast for some actions
    -Ex: when the user copies to the clipboard the Toast can be used
    -A parent with position relative is needed

---
+++ src/components/shared/ActionToast/ActionToast.tsx
@@ -0,0 +1,39 @@
+import { AnimatePresence, motion, Variants } from "framer-motion";
+
+type Props = {
+  show: boolean;
+  text: string;
+  wrapperClassName?: string;
+  textClassName?: string;
+};
+
+const toastContainer: Variants = {
+  hidden: { scale: 0, translateX: "50%", right: "50%" },
+  show: { scale: 1 },
+  exit: { opacity: 0, transition: { duration: 0.3 } },
+};
+
+const ActionToast = ({
+  show,
+  text,
+  wrapperClassName,
+  textClassName,
+}: Props) => {
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.div
+          variants={toastContainer}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+          className={`absolute bottom-[-30px] bg-purple-600 rounded-md flex items-center p-1 ${wrapperClassName || ""}`}
+        >
+          <span className={`font-bold text-xs ${textClassName || ""}`}>{text}</span>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default ActionToast;
