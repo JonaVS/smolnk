5a3ae2b33e51643ba8ce604a7a59036281de529f
JonaVS <jon4963@gmail.com>
2023-03-26 11:21:31 -0600

    refactor: use animation variants at ShortenerSection

--- src/components/ShortenerSection/ShortenerSection.tsx
+++ src/components/ShortenerSection/ShortenerSection.tsx
@@ -1,16 +1,28 @@
-import { LayoutGroup } from "framer-motion";
+import { LayoutGroup, Transition, Variants } from "framer-motion";
 import MainSectionWrapper from "../shared/MainSectionWrapper/MainSectionWrapper";
 import ShortenerSectionMsg from "./ShortenerSectionMsg/ShortenerSectionMsg";
 import ShortenerForm from "./ShortenerSectionForm/ShortenerForm";
+
+const transition: Transition = {
+  duration: 0.5,
+  layout: {type: "tween", duration: 0.3}
+}
+
+const section: Variants = {
+  hidden: { x: -100, opacity: 0},
+  show: { opacity: 1, x: 0 },
+  exit: { x: -100, opacity: 0 }
+};
 
 const ShortenerSection = () => {
   return (
     <LayoutGroup>
       <MainSectionWrapper
-        transition={{ duration: 0.5, layout: { type: "tween", duration: 0.3 } }}
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ x: -100, opacity: 0 }}
+        variants={section}
+        transition={transition}
+        initial="hidden"
+        animate="show"
+        exit="exit"
         layout="position"
       >
         <ShortenerSectionMsg />
