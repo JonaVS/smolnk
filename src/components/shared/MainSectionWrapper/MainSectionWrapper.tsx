85e75034c72fdbd2e883dcf55394e963c4e1a549
JonaVS <jon4963@gmail.com>
2023-03-25 18:57:05 -0600

    fix: prevent undefined CSS class in MainSectionWrapper

--- src/components/shared/MainSectionWrapper/MainSectionWrapper.tsx
+++ src/components/shared/MainSectionWrapper/MainSectionWrapper.tsx
@@ -2,9 +2,14 @@ import { HTMLMotionProps, motion } from "framer-motion";
 
 type Props = HTMLMotionProps<'section'>;
   
-const MainSectionWrapper = ({ children, ...props }: Props) => {
+const MainSectionWrapper = ({ children, className="", ...props }: Props) => {
   return (
-    <motion.section {...props} className={`m-auto w-full md:w-3/4 max-w-7xl ${props.className}`}>{children}</motion.section>
+    <motion.section
+      {...props}
+      className={`m-auto w-full md:w-3/4 max-w-7xl${className && ` ${className}`}`}
+    >
+      {children}
+    </motion.section>
   );
 };
 
