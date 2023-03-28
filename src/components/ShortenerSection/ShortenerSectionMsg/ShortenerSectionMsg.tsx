7d956d676cbd742189863bd656d7cbd5627e3216
JonaVS <jon4963@gmail.com>
2023-03-28 14:13:35 -0600

    refactor: remove repetitions at ShortenerSectionMsg
    -Improvements of Tailwind classes

--- src/components/ShortenerSection/ShortenerSectionMsg/ShortenerSectionMsg.tsx
+++ src/components/ShortenerSection/ShortenerSectionMsg/ShortenerSectionMsg.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 const lineClassName = "mx-auto text-center h-max w-full text-white text-2xl md:text-5xl lg:text-6xl font-bold";
 const keywordClassName = "font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-teal-200";
+const extraLineClassName = "text-center text-gray-50 text-xs sm:text-sm font-bold" 
+const emojiClassName = "text-xl"
 
 const ShortenerSectionMsg = () => {
   return (
@@ -12,16 +14,16 @@ const ShortenerSectionMsg = () => {
       <p className={lineClassName}>
         we're talking about <span className={keywordClassName}>URLs</span>{" "}
       </p>
-      <p className="text-center mt-2 px-2 text-gray-50 text-xs sm:text-sm font-bold">
-        Small URLs with just one click. Shorten your URL below
-        <span className="text-xl"> 😉</span>
+      <p className={`${extraLineClassName} mt-2`}>
+        Short URLs with just one click. Shorten your URL below
+        <span className={emojiClassName}> 😉</span>
       </p>
-      <p className="mx-auto text-center text-gray-50 text-xs sm:text-sm font-bold">
+      <p className={extraLineClassName}>
         Already have one? Click{" "}
         <Link to="/url-usage">
           <span className="text-teal-200 font-bold">here</span>
         </Link>{" "}
-        to see URL usage count <span className="text-xl">👀</span>
+        to see URL usage count <span className={emojiClassName}>👀</span>
       </p>
     </>
   );
