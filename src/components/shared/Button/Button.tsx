f0a77972f809caf72c58199d85d7365b2a467bed
JonaVS <jon4963@gmail.com>
2023-03-29 12:39:50 -0600

    ui: update Button component animations
    -A tap animation has been added
    -This removes opacity transition (bugged in some devices)

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,21 +1,23 @@
+import { motion, HTMLMotionProps } from "framer-motion";
 
 export const ButtonVariant = {
   default: 'p-3 text-gray-50 bg-purple-600 rounded-lg',
-  formPrimary: "px-3 text-gray-50 font-bold bg-purple-600 rounded-r-xl drop-shadow-2xl transition-opacity duration-200 ease-in-out hover:opacity-75 disabled:opacity-50 disabled:pointer-events-none",
+  formPrimary: "px-3 text-gray-50 font-bold bg-purple-600 rounded-r-xl drop-shadow-2xl disabled:opacity-50 disabled:pointer-events-none",
 } as const
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type Props = HTMLMotionProps<'button'> & {
   variant?:  keyof typeof ButtonVariant
 };
 
-const Button = ({variant = "default", className="", ...props}:Props) => {
+const Button = ({ variant = "default", className = "", ...props }: Props) => {
   return (
-    <button
-    {...props}
+    <motion.button
+      whileTap={{ scale: 0.9 }}
+      {...props}
       className={`${ButtonVariant[variant]}${className && ` ${className}`}`}
     >
       {props.children}
-    </button>
+    </motion.button>
   );
 };
 
