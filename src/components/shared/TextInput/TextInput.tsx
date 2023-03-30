31c938026df545b86dab68e02849cd0fc7ea0074
JonaVS <jon4963@gmail.com>
2023-03-30 12:42:01 -0600

    ui: change TextInput focus styles
    -Updated focus styles to change bg color

--- src/components/shared/TextInput/TextInput.tsx
+++ src/components/shared/TextInput/TextInput.tsx
@@ -12,7 +12,8 @@ const TextInput = ({ label, labelClassName, id, ...props }: Props) => {
       <input
         id={id}
         type="text"
-        className="max-w-sm md:max-w-lg w-full h-14 px-3 bg-gray-50 text-gray-500 font-medium rounded-l-xl rounded-r-none drop-shadow-lg focus:outline-none"
+        className="max-w-sm md:max-w-lg w-full h-14 px-3 bg-gray-50 text-gray-500 font-medium rounded-l-xl rounded-r-none 
+          drop-shadow-lg focus:outline-none focus:bg-slate-100 transition-colors"
         {...props}
       />
     </>
