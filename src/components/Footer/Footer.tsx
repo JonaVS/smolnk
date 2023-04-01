97cb03c924f80254589876935262fc3d45901791
JonaVS <jon4963@gmail.com>
2023-04-01 15:32:51 -0600

    ui: add hover animation to Footer link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,22 @@
+import { motion, Variants } from "framer-motion";
+
+const link: Variants = {
+  hover: {scale: 0.9, transition: {type: "spring", duration: 0.5}}
+}
+
 const Footer = () => {
   return (
     <footer className="w-full h-10 flex justify-center place-items-center text-gray-50 font-bold mt-auto p-1">
       Made by&nbsp;
-      <a
+      <motion.a
+        variants={link} whileHover="hover"
         href="https://github.com/JonaVS"
         target="_blank"
         rel="noopener noreferrer"
         className="text-teal-200"
       >
         JonaVS
-      </a>
+      </motion.a>
     </footer>
   );
 };
