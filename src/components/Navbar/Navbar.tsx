1525d3ac330158e67dda90c8c7f7f4f7f12cd289
JonaVS <jon4963@gmail.com>
2023-04-01 15:32:21 -0600

    ui: add hover animation to Navbar items
    -Hover animation for app logo
    -Hover animation for Github Icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,22 @@
-import appLogo from "../../assets/appLogo.png";
+import { motion, Variants } from "framer-motion";
 import { Link } from "react-router-dom";
+import appLogo from "../../assets/appLogo.png";
 import { BsGithub } from "react-icons/bs";
+
+const navItem: Variants = {
+  hover: {scale: 0.9, transition: {type: "spring"}}
+} 
 
 const Navbar = () => {
   return (
     <nav className="h-16 flex items-center w-full fixed backdrop-blur-sm">
       <ul className="w-11/12 md:w-4/5 m-auto list-none flex justify-between items-center">
-        <li>
+        <motion.li variants={navItem} whileHover="hover">
           <Link to="/">
             <img src={appLogo} className="h-6" alt="App logo" />
           </Link>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={navItem} whileHover="hover">
           <a
             href="https://github.com/JonaVS/smolnk"
             target="_blank"
@@ -19,7 +24,7 @@ const Navbar = () => {
           >
             <BsGithub size={25} color="white" />
           </a>
-        </li>
+        </motion.li>
       </ul>
     </nav>
   );
